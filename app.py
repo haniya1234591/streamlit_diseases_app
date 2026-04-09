51e2f0ffc5b9ce324eec2440cba3e5241d47680c
haniya1234591 <haniyaa1310@gmail.com>
2026-04-09 18:53:54 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 
 base_path = os.path.dirname(__file__)
 model = pickle.load(open(os.path.join(base_path, 'model/model.pkl'), 'rb'))
-scaler_model = pickle.load(open(os.path.join(base_path, 'model/scaler.pkl'), 'rb'))
+scaler = pickle.load(open(os.path.join(base_path, 'model/scaler.pkl'), 'rb'))
 
 st.title("Diabetes Prediction Application")
 st.write("Please enter patient disease to predict risk of diabetes")
